ea6bcf47bfdbbb0968e7bf6db2cc9265efd8a20f
Md Minhazul Arifin <69733252+Arifin-rafe@users.noreply.github.com>
2025-12-13 01:47:40 +0600

    feat: implement Teacher class extending Student; add StudentInterface with default values

--- src/class.js
+++ src/class.js
@@ -10,6 +10,24 @@ class Student {
         this.skill = skill;
     }
 }
+class Teacher extends Student {
+    constructor(name, age, skill) {
+        super(name, age, skill);
+    }
+    ageCalc() {
+        return this.age + 5;
+    }
+}
 const student1 = new Student("John", 20, true);
 console.log(student1);
+const teacher1 = new Teacher("Jane", 30, true);
+console.log(teacher1.ageCalc());
+console.log(teacher1);
+class StudentInterface {
+    name = "John";
+    age = 30;
+    skill = false;
+}
+const student2 = new StudentInterface();
+console.log(student2);
 //# sourceMappingURL=class.js.map
--- src/class.ts
+++ src/class.ts
@@ -1,12 +1,39 @@
 class Student {
-  name: string;
-  age: number;
-  skill: boolean
+  public name: string;
+  public age: number;
+  public skill: boolean;
   constructor(name: string, age: number, skill: boolean) {
     this.name = name;
     this.age = age;
     this.skill = skill;
   }
 }
+
+class Teacher extends Student {
+  constructor(name: string, age: number, skill: boolean) {
+    super(name, age, skill);
+  }
+  ageCalc() {
+    return this.age + 5;
+  }
+}
 const student1 = new Student("John", 20, true);
 console.log(student1);
+const teacher1 = new Teacher("Jane", 30, true);
+console.log(teacher1.ageCalc());
+console.log(teacher1);
+
+// interface
+interface StudentInterface {
+  name: string;
+  age: number;
+  skill: boolean;
+}
+
+class StudentInterface implements StudentInterface {
+  public name: string = "John";
+  public age: number = 30;
+  public skill: boolean = false;
+}
+const student2 = new StudentInterface();
+console.log(student2);
